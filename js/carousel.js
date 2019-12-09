0190ee27c6192f07fe5496f3f6d09127b26e5281
julian <julisanchez1991@gmail.com>
2019-12-08 21:46:02 -0300

    08/12/19

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay: 500,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
